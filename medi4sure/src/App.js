88fe41a5a8eca2f7cd0673b8ae602da42939c1f5
mohammedowaisnoman <89965483+mohammedowaisnoman@users.noreply.github.com>
2021-10-30 23:19:56 +0530

    hhhgg

--- medi4sure/src/App.js
+++ medi4sure/src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import { Home } from "./Components/Home/Home";
+import { BrowserRouter as Route,Switch, Link } from "react-router-DOM";
+import { Feedback } from "./Components";
+import "./App.css";
+export const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <Router>
+    <Link to="/">Back To Home</Link>
+    <Link to="/emicalc">EMI Calculator</Link>
+    <Home />                                                                                                        
 
-export default App;
+    </Router>         
+  );
+};
